7eb99e850ac641406ce83d5e047e498807ae373b
John Running <johnrunning@johns-mbp.lan>
2019-08-18 16:04:00 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <div class="row">
+            <VideoDetail :video="selectedVideo" class="col-lg-8"/>
+            <VideoList @videoSelect="onVideoSelect" :videos="videos" class="col-lg-4"></VideoList>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+
+const API_KEY = 'AIzaSyB0ToJFPdt3Dp2stb8ynXqHkNe9U0ivB0c';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        SearchBar,
+        VideoList,
+        VideoDetail
+    },
+    data() {
+        return { 
+            videos: [],
+            selectedVideo: null
+        };
+    },
+    methods: {
+        // same as onTermChange: function() {} (es6 syntax)
+        onTermChange(searchTerm) {
+            console.log(searchTerm)
+            //initiate search for videos from youtube api
+            axios.get('https://www.googleapis.com/youtube/v3/search', {
+                params: {
+                    part: 'snippet',
+                    maxResults: 3,
+                    key: API_KEY,
+                    type: 'video',
+                    q: searchTerm
+                }
+            }).then((response) => {
+                this.videos = response.data.items;
+                this.selectedVideo = response.data.items[0];
+            });
+        },
+        onVideoSelect(video){
+            this.selectedVideo = video;
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
